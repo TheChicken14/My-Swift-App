8b5ce7240330bd1cbe05fb65b3f22bf218b28ea1
Wisse <wissehes@gmail.com>
2020-08-05 16:39:24 +0200

    Initial Commit

---
+++ My App/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  My App
+//
+//  Created by Wisse Hes on 05/08/2020.
+//  Copyright © 2020 Wisse Hes. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
