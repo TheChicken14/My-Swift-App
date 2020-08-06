4a18e86fa816846644436b2a9b51e89eaf8966c3
Wisse <wissehes@gmail.com>
2020-08-06 12:49:57 +0200

    Made this thingy :relieved:

--- My App/ViewController.swift
+++ My App/ViewController.swift
@@ -10,6 +10,21 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var textField1: NSTextField!
+    @IBOutlet weak var textField2: NSTextField!
+    
+    @IBOutlet weak var label: NSTextField!
+    
+    @IBAction func buttonClicked(_ sender: Any) {
+        if Int(textField1.stringValue) == nil || Int(textField2.stringValue) == nil  {
+            label.stringValue = "Both values should be a number!"
+        } else {
+            var num = Int(textField1.stringValue)
+            num! += Int(textField2.stringValue)!
+            
+            label.integerValue = num!
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
